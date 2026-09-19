4601da19bfb3b01b7b527b286686f0d9b60bed6a
User123573 <wolfdark1982@hotmail.com>
2026-09-19 13:12:40 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const host = 'localhost';
 const port = 8000;
 const requestListener = function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from my JavaScript server!');
+    res.end();
 };
 const server = http.createServer(requestListener);
